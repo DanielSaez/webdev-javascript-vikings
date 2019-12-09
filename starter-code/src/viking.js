5747c64bae7bacb95c9cc7a5c22f25a0d4a1bbc1
Saez Daniel <20318@columbushs.com>
2019-12-09 13:46:53 -0500

    notdone

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,23 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength
+    }
+
+    reciveDamage(){
+        this.health = this.health - hitHealth
+    }
+}
 
 // Viking
-class Viking {}
+class Viking {
+    constructor
+
+}
 
 // Saxon
 class Saxon {}
